434d91b30dcb88d9d7ec0dafe793de39047feba2
PURUSHOTTAM <77604098+PURHSHOTTAM@users.noreply.github.com>
2021-05-04 20:20:52 +0530

    Update config.py

--- userge/config.py
+++ userge/config.py
@@ -170,4 +170,3 @@ def hbot_version(tag: str) -> str:
             except JSONDecodeError:
                 pass
     return f"{tag}|{tag_name or ''}{commits or ''}@{pref_branch or branch or 'alpha'}"
-© 2021 GitHub, Inc.
