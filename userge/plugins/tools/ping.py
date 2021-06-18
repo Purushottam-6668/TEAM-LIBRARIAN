a47b70a272553b5a726402cc13f0c2167f0c7df1
PURUSHOTTAM <77604098+Purushottam-6668@users.noreply.github.com>
2021-06-18 17:42:37 +0530

    Update ping.py

--- userge/plugins/tools/ping.py
+++ userge/plugins/tools/ping.py
@@ -33,7 +33,7 @@ async def pingme(message: Message):
         m_s = round((t_m_s - 0.6) / 3, 3)
         await message.edit(f"**「⚡Average Pong!࿐**\n`{m_s} ms`")
     else:
-        await message.edit("`Pong!`")
+        await message.edit("`😺PONG!`")
         end = datetime.now()
         m_s = (end - start).microseconds / 1000
         await message.edit(f"**「⚡Pong࿐**\n`{m_s} ms`")
